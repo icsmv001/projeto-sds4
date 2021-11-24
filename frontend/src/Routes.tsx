a0fbc43f75658712ab988ef2b59d97e140045fef
icsmv001 <icsmv001@gmail.com>
2021-11-23 23:20:20 -0300

    criar pgans home e dasboard e resolver erro de importacao do Swith, usando versao 5.0.0 do router

---
+++ frontend/src/Routes.tsx
@@ -0,0 +1,22 @@
+import Dashboard from "pages/Dashboard";
+import Home from "pages/Home";
+import React, { Route, Switch } from "react-router";
+import { BrowserRouter } from "react-router-dom";
+
+const Routes = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+
+        <Route path="/dashboard">
+          <Dashboard />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
+};
+
+export default Routes;
