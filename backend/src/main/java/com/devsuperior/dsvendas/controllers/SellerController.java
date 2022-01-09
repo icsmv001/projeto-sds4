6dbfd0897ee843794354fc1f0c826b30c1355499
icsmv001 <icsmv001@gmail.com>
2022-01-09 19:32:13 -0300

    CONSULTA VENDEDOR PAGINA - OK

--- backend/src/main/java/com/devsuperior/dsvendas/controllers/SellerController.java
+++ backend/src/main/java/com/devsuperior/dsvendas/controllers/SellerController.java
@@ -56,79 +56,79 @@ public class SellerController {
 	
 	
 	
-	
-	// metodo para inserir novo registro de vendedor - sucesso - 15/12/2021
-	@PostMapping(value = "/vendedor")
-
-	public Seller adicionar(@RequestBody Seller seller) {
-		return sellerRepository.save(seller);
-
-	}
-
-	/// teste para retornar por id vendedor.
-
-	@GetMapping(value = "/vendedorId/{id}")
-	public ResponseEntity<String> busca(@PathVariable Long id) {
-		// System.out.println("TESTE ## PASSANDO POR AQUI 1 !!!!! ##### " + id);
-
-		Optional<Seller> seller = sellerRepository.findById(id);
-		// System.out.println("TESTE ## PASSANDO POR AQUI 2 !!!!! ##### " +
-		// seller.get().getName());
-		if (seller.isPresent()) {
-			return ResponseEntity.ok(seller.get().getName());
-
-		}
-		return ResponseEntity.notFound().build();
-
-	}
-
-	/// -------
-	/// teste para retornar por id vendedor.
-
-	@GetMapping(value = "/vendedorNm/{name}")
-	public ResponseEntity<String> buscaNome(@PathVariable String name) {
-		// System.out.println("TESTE ## PASSANDO POR AQUI 1 !!!!! ##### " + name);
-
-		Optional<Seller> seller = sellerRepository.findByName(name);
-		// System.out.println("TESTE ## PASSANDO POR AQUI 2 !!!!! ##### " +
-		// seller.get().getName());
-		if (seller.isPresent()) {
-			return ResponseEntity.ok(seller.get().getName());
-
-		}
-		return ResponseEntity.notFound().build();
-
-	}
-
-	@GetMapping(value = "/vendedoresNm/{name}")
-
-	public ResponseEntity<List<SellerDTO>> buscarNomes(@PathVariable String name) {
-
-		// System.out.println("TESTE ## PASSANDO POR AQUI 1 !!!!! ##### " + name);
-		List<SellerDTO> list = service.findByNameContaining(name);
-		return ResponseEntity.ok(list);
-
-	}
-
-	// alteracao de nome por id
-	@PutMapping(value = "/AtualizaNome/{id}")
-	public ResponseEntity<String> Atualizar(@PathVariable Long id, @RequestBody Seller seller) {
-
-		// System.out.println("TESTE ## PASSANDO POR AQUI alteracao 1 !!!!! ##### " +
-		// id);
-
-		if (!sellerRepository.existsById(id)) {
-			return ResponseEntity.notFound().build();
-		}
-
-		seller.setId(id);
-		seller = sellerRepository.save(seller);
-
-		// System.out.println("TESTE ## PASSANDO POR AQUI alteracao 2 !!!!! ##### " +
-		// id);
-
-		return ResponseEntity.ok(seller.getName());
-
-	}
+//	
+//	// metodo para inserir novo registro de vendedor - sucesso - 15/12/2021
+//	@PostMapping(value = "/vendedor")
+//
+//	public Seller adicionar(@RequestBody Seller seller) {
+//		return sellerRepository.save(seller);
+//
+//	}
+//
+//	/// teste para retornar por id vendedor.
+//
+//	@GetMapping(value = "/vendedorId/{id}")
+//	public ResponseEntity<String> busca(@PathVariable Long id) {
+//		// System.out.println("TESTE ## PASSANDO POR AQUI 1 !!!!! ##### " + id);
+//
+//		Optional<Seller> seller = sellerRepository.findById(id);
+//		// System.out.println("TESTE ## PASSANDO POR AQUI 2 !!!!! ##### " +
+//		// seller.get().getName());
+//		if (seller.isPresent()) {
+//			return ResponseEntity.ok(seller.get().getName());
+//
+//		}
+//		return ResponseEntity.notFound().build();
+//
+//	}
+//
+//	/// -------
+//	/// teste para retornar por id vendedor.
+//
+//	@GetMapping(value = "/vendedorNm/{name}")
+//	public ResponseEntity<String> buscaNome(@PathVariable String name) {
+//		// System.out.println("TESTE ## PASSANDO POR AQUI 1 !!!!! ##### " + name);
+//
+//		Optional<Seller> seller = sellerRepository.findByName(name);
+//		// System.out.println("TESTE ## PASSANDO POR AQUI 2 !!!!! ##### " +
+//		// seller.get().getName());
+//		if (seller.isPresent()) {
+//			return ResponseEntity.ok(seller.get().getName());
+//
+//		}
+//		return ResponseEntity.notFound().build();
+//
+//	}
+//
+//	@GetMapping(value = "/vendedoresNm/{name}")
+//
+//	public ResponseEntity<List<SellerDTO>> buscarNomes(@PathVariable String name) {
+//
+//		// System.out.println("TESTE ## PASSANDO POR AQUI 1 !!!!! ##### " + name);
+//		List<SellerDTO> list = service.findByNameContaining(name);
+//		return ResponseEntity.ok(list);
+//
+//	}
+//
+//	// alteracao de nome por id
+//	@PutMapping(value = "/AtualizaNome/{id}")
+//	public ResponseEntity<String> Atualizar(@PathVariable Long id, @RequestBody Seller seller) {
+//
+//		// System.out.println("TESTE ## PASSANDO POR AQUI alteracao 1 !!!!! ##### " +
+//		// id);
+//
+//		if (!sellerRepository.existsById(id)) {
+//			return ResponseEntity.notFound().build();
+//		}
+//
+//		seller.setId(id);
+//		seller = sellerRepository.save(seller);
+//
+//		// System.out.println("TESTE ## PASSANDO POR AQUI alteracao 2 !!!!! ##### " +
+//		// id);
+//
+//		return ResponseEntity.ok(seller.getName());
+//
+//	}
 
 }
