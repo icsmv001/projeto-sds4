b27522c17eaecf9a0621f6d559f64c0932f35602
icsmv001 <icsmv001@gmail.com>
2022-01-20 22:42:49 -0300

    testes com submit, no fron end em typescript, para criar formalario de cadastro de vendeor

--- frontend/src/pages/pagina03/index.tsx
+++ frontend/src/pages/pagina03/index.tsx
@@ -3,12 +3,20 @@ import NavBar from "components/navbar";
 import React from "react";
 
 const Pagina03 = () => {
+  const handleSubmit = (event: { preventDefault: () => void; target: any }) => {
+    event.preventDefault();
+    const NmVendedor1 = (event.target as any).NmVendedor1.value;
+    const datacadastro = (event.target as any).datacadastro.value;
+
+    console.log(NmVendedor1, datacadastro);
+  };
+
   return (
     <>
-      <NavBar />
+      <form className=" container formVendas" onSubmit={handleSubmit}>
+        <NavBar />
 
-      <div className="container formVendas">
-        <div className="jumbotron">
+        {/* <div className="jumbotron">
           <h1 className="display-4">Cadastro de Vendedor</h1>
           <p className="container">
             <div className="row">
@@ -31,6 +39,14 @@ const Pagina03 = () => {
                   placeholder="Digite data Evento"
                 ></input>
               </div>
+
+              <label htmlFor="NmVendedor">Digite Nome Vendedor:</label>
+              <input
+                type="NmVendedor"
+                className="form-control"
+                id="NmVendedor1"
+                placeholder="Digite Nome Vendedor"
+              />
             </div>
           </p>
 
@@ -38,11 +54,35 @@ const Pagina03 = () => {
             <div className="row"></div>
           </p>
 
-          <button className="btn btn-success" type="submit">
+          <button type="submit" className="btn btn-success">
+            Salvar
+          </button>
+        </div> */}
+
+        <h1 className="display-4">Cadastro de Vendedor</h1>
+
+        <div className="teste">
+          <label htmlFor="NmVendedor">Digite Nome Vendedor:</label>
+          <input
+            type="NmVendedor1"
+            className="form-control"
+            id="NmVendedor1"
+            placeholder="Digite Nome Vendedor"
+          ></input>
+
+          <label className="col-xs-2 control-label">Data: </label>
+          <input
+            type="date"
+            className="form-control"
+            id="datacadastro"
+            placeholder="Digite data Evento"
+          ></input>
+
+          <button type="submit" className="btn btn-success">
             Salvar
           </button>
         </div>
-      </div>
+      </form>
       <Footer />
     </>
   );
