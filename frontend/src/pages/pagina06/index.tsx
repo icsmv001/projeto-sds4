0493ca9afb6dd5ba6119726cd0dbbf394975d11c
icsmv001 <icsmv001@gmail.com>
2022-04-07 11:05:29 -0300

    atualizando marcacao de versao v001- rel sem paginacao-ok

--- frontend/src/pages/pagina06/index.tsx
+++ frontend/src/pages/pagina06/index.tsx
@@ -8,7 +8,7 @@ const Pagina06 = () => {
     <>
       <NavBar />
       <div className="container">
-        <h1 className="text-primary py-3">Lista de Vendas - ICSMV001</h1>
+        <h1 className="text-primary py-3">Lista de Vendas - ICSMV001 - v.01</h1>
         <h4>Sem Paginação </h4>
 
         <ListarVenda />
