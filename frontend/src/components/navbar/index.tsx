15a3cb7e6afda93b6dc9671e203536046a7857d0
icsmv001 <icsmv001@gmail.com>
2022-05-01 22:53:41 -0300

    atualiza COMPONENT NAVBAR

--- frontend/src/components/navbar/index.tsx
+++ frontend/src/components/navbar/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./styles.css";
 import ImgDsDark from "assets/img/ds-dark.svg";
-
 const NavBar = () => {
   return (
     <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom shadow-sm">
